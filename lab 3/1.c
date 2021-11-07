d2ca8caa12c2ecf01545954c3363d4e7b09d6af7
Maciej Niedźwiecki <minecraftczlek@gmail.com>
2021-11-07 15:07:56 +0100

    main

--- lab 3/1.c
+++ lab 3/1.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[]){
 
     unsigned int x=5, y=1;
-    // scanf("%d", &x);
+    scanf("%d", &x);
     for(int i=1; i<=x; i++){
         for(int j=0; j<i; j++){
             printf("%d ", y);
