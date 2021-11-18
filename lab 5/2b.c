4687b0825a5d9c3e3e0fd53ee82307f9f0c2d144
Maciej Niedźwiecki <minecraftczlek@gmail.com>
2021-11-18 11:00:23 +0100

    main

---
+++ lab 5/2b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define unsigned long long int xint
+// definiuje słowo kluczowe xint
+
+int silnia(int n){
+    if(n == 1) return 1;
+    // jeśli n==1 to po prostu zwracam 1
+    return silnia(n-1) * n;
+    // wywołuje rekurencyjnie funkcje i mnoże wynik * n
+}
+
+int main(){  
+    printf("%d", silnia(7)); 
+    // wywołujemu funkcję silnia i wypisujemy jej wynik  
+    return 0;
+}
