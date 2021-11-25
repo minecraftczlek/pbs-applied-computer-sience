117bde4dc2411995da2c2e52d5eb1aa6cfb832fb
Maciej Niedźwiecki <minecraftczlek@gmail.com>
2021-11-25 12:19:22 +0100

    m

--- lab 5/2b.c
+++ lab 5/2b.c
@@ -4,7 +4,7 @@ typedef unsigned long long int xint;
 // definiuje słowo kluczowe xint
 
 xint silnia(xint n){
-    if(n == 1) return 1;
+    if(n == 1 || n == 0) return 1;
     // jeśli n==1 to po prostu zwracam 1
     return silnia(n-1) * n;
     // wywołuje rekurencyjnie funkcje i mnoże wynik * n
