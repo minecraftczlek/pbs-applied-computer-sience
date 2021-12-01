853bbaab8b81b8c7bd9c07d0d9c304645ebd0a5a
minecraftczlek <36611575+minecraftczlek@users.noreply.github.com>
2021-12-01 15:30:51 +0100

    Create 2.c

---
+++ lab 4/2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define rate 40
+
+int main(){
+    float hours = 40, brutto = 0, netto;
+    scanf("%f", &hours);
+
+    brutto += (rate*40) + ((hours-40)*rate*1.5);
+
+    netto = brutto;
+
+    printf("Wynagrodzenie brutto: %1.2fzl,\n", brutto);
+
+    if(brutto<=1200){
+        netto -= brutto*0.15;
+        brutto = 0;
+    }else{
+        netto -= 1200*0.15;
+        brutto -= 1200;
+
+        if(brutto < 600){
+            netto -= brutto*0.2;
+            brutto = 0;
+        }
+        else{
+            netto -= 600*0.2;
+            brutto -= 600;
+            netto -= brutto * 0.25; 
+        }
+    }
+
+    printf("kwota netto wynosi: %1.2fzl\n",netto);
+
+    return 0;
+} 
