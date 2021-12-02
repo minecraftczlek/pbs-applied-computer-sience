8b7218171e3d274d8671852a0128fad2bcf0f0b4
Maciej Niedźwiecki <minecraftczlek@gmail.com>
2021-12-02 10:31:58 +0100

    m

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 <a href="lab 2">lab 2</a>,
 <a href="lab 3">lab 3</a>,
 <a href="lab 4">lab 4</a>,
-<a href="lab 5">lab 5</a>
+<a href="lab 5">lab 5</a>,
+<a href="lab 6">lab 6</a>
 <br>
 ### Wykłady:
 <a href="sedziowie">Sędziowie</a><br>
---
+++ lab 6/1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int table[5];
+    // deklaracja tablicy
+    for(int i=0; i<5; i++) scanf("%d", &table[i]);
+    // uzupełnianie tablicy
+    for(int i=4; i>=0; i--) printf("%d ", table[i]);
+    // wypisywanie tablicy w odwrotnej kolejności
+    return 0;
+}
