ebad26f9805fd27a1cbe34bba1dba90e89ce8542
Maciej Niedźwiecki <minecraftczlek@gmail.com>
2022-01-30 20:28:44 +0100

    fix 7/3

--- lab 7/3.c
+++ lab 7/3.c
@@ -21,5 +21,6 @@ int main(){
 
 void sklej(unsigned char *A, unsigned char *B){
     int size = strlen(A) - 1;
-    for(int i=0; i<strlen(B)-1; i++) A[size + i] = B[i];
+    for(int i=0; i<strlen(B) - 1; i++) A[size + i] = B[i];
+    A[size + strlen(B) - 1] = 0;
 }
