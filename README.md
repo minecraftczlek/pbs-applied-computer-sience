c59cd3fa4b58c44c8e4258cf5323b2275d0a95c9
Maciej Niedźwiecki <minecraftczlek@gmail.com>
2022-02-03 10:40:19 +0100

    10/1

--- README.md
+++ README.md
@@ -8,7 +8,8 @@
 <a href="lab 6">lab 6</a>,
 <a href="lab 7">lab 7</a>,
 <a href="lab 8">lab 8</a>,
-<a href="lab 9">lab 9</a>
+<a href="lab 9">lab 9</a>,
+<a href="lab 10">lab 10</a>
 <br>
 ### Wykłady:
 <a href="sedziowie">Sędziowie</a><br>
---
+++ lab 10/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int x;
+    printf("podaj x: ");
+    scanf("%d", &x);
+
+    if((x&16)!=0) printf("piaty bit ma wartosc 1\n");
+    else printf("piaty bit nie ma wartosci 1\n");
+
+    if((x&64)!=0) printf("Osmy bit ma wartosc 1\n");
+    else printf("osmy bit nie ma wartosci 1\n");
+    
+    
+    return 0;
+}
