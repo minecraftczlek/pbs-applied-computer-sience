6800c91f36a5f6fb2f00e761b88a1cab8ff37fbe
Maciej Niedźwiecki <minecraftczlek@gmail.com>
2022-02-03 10:47:16 +0100

    10/1

--- lab 10/1.c
+++ lab 10/1.c
@@ -5,11 +5,14 @@ int main(){
     printf("podaj x: ");
     scanf("%d", &x);
 
-    if((x&16)) printf("piaty bit ma wartosc 1\n");
+    if(x&16) printf("piaty bit ma wartosc 1\n");
     else printf("piaty bit nie ma wartosci 1\n");
 
-    if((x&64)) printf("Osmy bit ma wartosc 1\n");
+    if(x&128) printf("Osmy bit ma wartosc 1\n");
     else printf("osmy bit nie ma wartosci 1\n");
+
+    if((x&16) && (x&128)) printf("oby dwa bity maja wartosc 1\n");
+    else printf("oby dwa bity nie maja wartosci 1\n");
     
     
     return 0;
