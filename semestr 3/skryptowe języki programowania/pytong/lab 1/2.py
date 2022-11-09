1fb86361ba1cc2a57fd6f3f3c797344da023666e
Maciej Niedźwiecki <minecraftczlek@gmail.com>
2022-11-09 11:00:15 +0100

    generic commit

--- semestr 3/skryptowe języki programowania/pytong/lab 1/2.py
+++ semestr 3/skryptowe języki programowania/pytong/lab 1/2.py
@@ -1,19 +1,15 @@
-import string
-
 lista1 = []
 lista2 = []
 
 rozmiar1 = int(input("Podaj rozmiar 1 listy: "))
-for i in range(0,rozmiar1):
-    # lista1[i] = int(input("Podaj element nr, " + string(i) + ": "))
+for i in range(rozmiar1):
     lista1.append(int(input("Podaj element: ")))
 suma1 = sum(lista1)
 print("suma1: ", suma1)
 
 
 rozmiar2 = int(input("Podaj rozmiar 2 listy: "))
-for i in range(0,rozmiar2):
-    # lista2[i] = input("Podaj element nr, " + string(i) + ": ")
-    lista2.append(input("Podaj element nr: "))
+for i in range(rozmiar2):
+    lista2.append(input("Podaj element: "))
 suma2 = "".join(lista2)
 print("suma2: ", suma2)
