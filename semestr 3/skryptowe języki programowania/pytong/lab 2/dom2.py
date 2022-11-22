f36128c28b4749f0df861723ac36fdac58264c70
Maciej Niedźwiecki <minecraftczlek@gmail.com>
2022-11-22 16:22:44 +0100

    generic commit

--- semestr 3/skryptowe języki programowania/pytong/lab 2/dom2.py
+++ semestr 3/skryptowe języki programowania/pytong/lab 2/dom2.py
@@ -4,21 +4,17 @@ class drink:
         self.price = price
         self.percent = percent
         self.capacity = capacity
-    def __add__(self, o):
+    def __add__(self, o): #operator dodawania
         name = self.name + " z " + o.name
         price = self.price + o.price
         capacity = self.capacity + o.capacity
         percent = (self.percent/100*self.capacity + o.percent/100*o.capacity) / capacity * 100
         return drink(name, percent, price, capacity)
-    def __mul__(self, n):
-        return drink(self.name, self.percent, self.price*n, self.capacity*n)
-    def str(self):
-        print("name: ", self.name)
-        print("price: ",self.price)
-        print("percent: ",self.percent)
-        print("capacity: ",self.capacity)
-        print("------------------")
-    def __lt__(self, o):
+    def __mul__(self, n): #operator mnożenia
+        return drink(self.name, self.percent, self.price*n, self.capacity*n)  
+    def __str__(self): #print
+        return f"<name: {self.name} | price: {self.price} | percent: {self.percent} | capacity: {self.capacity}>"
+    def __lt__(self, o): #operator <
         r1 = 0
         r2 = 0
         if self.price != 0:
@@ -35,10 +31,10 @@ d.append(drink("cola", 0, 2, 200)) #2
 d.append(drink("lód", 0, 0, 33))   #3
 
 d.append(d[0]+d[1]+d[2]+d[3]) #4
-d[4].str()
+print(d[4])
 
 d.append(d[0]*5) #5
-d[5].str()
+print(d[5])
 
 d.append(drink("whiskey", 70, 12, 50))    #6
 d.append(drink("spirytus", 98, 25, 100))   #7
@@ -47,5 +43,7 @@ d.append( d[6] + d[2] + d[3] )    #8
 d.append( d[7] + d[3] )     #9
 
 d.sort()
-d[-1].str()
+print("posortowana tablica:")
+for x in d:
+    print(x)
 
